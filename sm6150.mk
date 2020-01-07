bd58b6ba6e35737cfad216f77aee809978673b03
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2020-01-08 00:06:25 +0100

    sm6150-common: Build ParanoidDoze
    Change-Id: I15aa87442124d30d6f69eb8d2c10ce47d26475d5

--- sm6150.mk
+++ sm6150.mk
@@ -105,10 +105,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-# Device-specific settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Dexpreopt
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUI
