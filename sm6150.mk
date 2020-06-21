28bff67f065bef171d45532cb0fa67f4cbfc42a5
Arian <arian.kulmer@web.de>
2020-06-21 20:39:14 +0200

    sm6150-common: Do not explicitly build telephony packages
    Change-Id: Ia82b3cc1a2ab03df45923c64d322b754de458b77

--- sm6150.mk
+++ sm6150.mk
@@ -198,8 +198,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
 
 # QTI
-PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect.vendor
 TARGET_COMMON_QTI_COMPONENTS := \
     adreno \
     bt \
@@ -243,18 +241,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi
 
 # Telephony
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml \
-    qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml \
-    qti-telephony-utils \
-    qti_telephony_utils.xml \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
