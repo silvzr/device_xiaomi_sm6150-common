2583d6fe88af9857699e8e91c6581a19d1cd34a6
silvzr <117378129+silvzr@users.noreply.github.com>
2022-12-10 00:05:29 +0100

    fix cleanup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,7 +130,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
